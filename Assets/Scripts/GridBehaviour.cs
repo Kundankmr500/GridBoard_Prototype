7c58c3de17661fb86caafdd3a49e402419d18810
Kundan kumar <30592698+Kundankmr500@users.noreply.github.com>
2020-02-13 17:44:07 +0530

    Initial commit

---
+++ Assets/Scripts/GridBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GridBehaviour : MonoBehaviour
+{
+    public float X_Start, Z_Start;
+    public int ColumnLength, RowLength;
+
+    public float X_Space, Z_Space;
+
+    public GameObject GridPrefab;
+    
+    private void Awake()
+    {
+        if (GridPrefab)
+        {
+            GenerateGrid();
+        }
+        else
+        {
+            Debug.Log("Missing GridPrefab, please assign gridPrefab");
+        }
+    }
+    
+    public void GenerateGrid()
+    {
+        for (int i = 0; i < ColumnLength * RowLength; i++)
+        {
+            GameObject obj = Instantiate(GridPrefab, new Vector3(X_Start + (X_Space * (i % ColumnLength)), 0.01f ,Z_Start + (Z_Space * (i / ColumnLength))),
+                Quaternion.Euler(90,0,0));
+            obj.transform.SetParent(transform);
+        }
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# NewSpaceResearch-Test
+ 
